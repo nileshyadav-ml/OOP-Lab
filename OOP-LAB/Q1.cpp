745a6ef769fc1bc76d7155d1bae2c026657dc9d7
nilesh <contact.nileshy@gmail.com>
2023-02-24 21:36:20 +0530

    finished Q1

--- OOP-LAB/Q1.cpp
+++ OOP-LAB/Q1.cpp
@@ -1,3 +1,35 @@
 //
-// Created by conta on 24-02-2023.
+// Created by Nilesh on 24-02-2023.
 //
+/*
+ Q1- Write a C++ program to find the sum of individual digit of a positive integer
+ * */
+
+#include<iostream>
+
+using namespace std;
+
+// declaration of add function
+int add(int num);
+
+
+// main function
+int main() {
+    int num;
+    cout << "Enter value of num : ";
+    cin >> num;
+    cout << "sum of " << num << " : " << add(num) << endl;
+    return 0;
+}
+
+// definition of add function
+int add(int num){
+    int reminder;
+    int sum = 0;
+    while(num!=0) {
+        reminder = num % 10;
+        sum += reminder;
+        num = num / 10;
+    }
+    return sum;
+}
