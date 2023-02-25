e5bd2bc0e70887df6fad55b45d872d05ee9facf5
Nilesh Kumar Yadav <84938479+nileshyadav-ml@users.noreply.github.com>
2023-02-25 17:36:01 +0530

    Update Q5.cpp

--- OOP-LAB/Q5.cpp
+++ OOP-LAB/Q5.cpp
@@ -3,21 +3,31 @@
 //
 // Q5 : Write a C++ program illustrating user-defined string processing functions using pointers to concatenate two strings.
 
-#include<iostream>
+#include <iostream>
+#define MAX_SIZE 100
 using namespace std;
-// main function
+
+// concat function to concatenate two strings
+void concat(char * s1, char * s2){
+    // loop will continue till it is not null
+    while(*(++s1));
+    
+    // using while loop to copy characters of second string pointed as 's2' at the end of first string pointed as 's2'
+    while(*(s1++) = *(s2++));
+} 
+
 int main() {
-    //declare s1 and s2
-    string s1;
-    string s2;
-    // input s1
-    cout<<"enter first string: ";
-    cin>>s1;
-    //input s2
-    cout<<"enter second string: ";
-    cin>>s2;
-    // concatenate sl and s2
-    s1+=s2;
-    cout<<"The concatenated string is "<<s1;
+ 
+    char str1[MAX_SIZE], str2[MAX_SIZE];
+    char * s1 = str1;
+    char * s2 = str2;
+    cout<<"Enter 1st string: ";
+    cin>>str1;
+    cout<<"Enter 2nd string: ";
+    cin>>str2;
+    concat(s1, s2);
+    cout<<"Concatenated string:"<<str1<<endl;
+ 
     return 0;
+ 
 }
