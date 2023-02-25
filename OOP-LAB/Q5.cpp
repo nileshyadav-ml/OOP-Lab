09312ade913eb714d0ff17028bfbe3c7eb38b537
Nilesh <contact.nileshy@gmail.com>
2023-02-25 13:13:09 +0530

    finished Q5

--- OOP-LAB/Q5.cpp
+++ OOP-LAB/Q5.cpp
@@ -1,3 +1,22 @@
 //
 // Created by Nilesh on 25-02-2023.
 //
+// Q5 : Write a C++ program illustrating user-defined string processing functions using pointers to concatenate two strings.
+
+#include<iostream>
+using namespace std;
+// main function
+int main() {
+    //declare s1 and s2
+    string s1;
+    string s2;
+    // input s1
+    cout<<"enter first string: ";
+    cin>>s1;
+    //input s2
+    cout<<"enter second string: ";
+    cin>>s2;
+    // concatenate sl and s2
+    s1+=s2;
+    cout<<"The concatenated string is "<<s1; return 0;
+}
